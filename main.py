0c40d5ce06252878c84846355c018717144303be
GemeHome <83622587+papigeme@users.noreply.github.com>
2024-10-21 08:31:05 -0600

    Update main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 def sum(a,b,c):
   c = a + b
   return c
-Resultado = 0
-number_1 = input('Ingrese el numero 1')
-number_2 = input('Ingrese el numero 2')
+  
 
-print(sum(number_1,number_2,Resultado))
+number_1 = int(input('Ingrese el numero 1:'))
+number_2 = int(input('Ingrese el numero 2:'))
+
+print('El resultado de la suma es:',sum(number_1,number_2,Resultado))
 
 
