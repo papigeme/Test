108122aaed11d67e06063dcc546dd780d55ed038
GemeHome <83622587+papigeme@users.noreply.github.com>
2024-10-21 08:23:46 -0600

    Update and rename main to main

---
+++ main.py
@@ -0,0 +1,10 @@
+def sum(a,b,c):
+  c = a + b
+  return c
+Resultado = 0
+number_1 = input('Ingrese el numero 1')
+number_2 = input('Ingrese el numero 2')
+
+print(sum(number_1,number_2,Resultado))
+
+
